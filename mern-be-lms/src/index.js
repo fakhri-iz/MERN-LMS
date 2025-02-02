65db6fbbe877542f3fb1e86ec3bdb1af73e9002f
fakhri-iz <fakhriikhwnauzaki@gmail.com>
2025-02-02 14:07:51 +0700

    first commit

---
+++ mern-be-lms/src/index.js
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import bodyParser from "body-parser";
+
+import globalRoutes from "./routes/globalRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
+import connectDB from "./utils/database.js";
+import paymentRoutes from "./routes/paymentRoutes.js";
+import courseRoutes from "./routes/courseRoute.js";
+
+const app = express();
+
+dotenv.config();
+
+connectDB();
+
+const port = 3000;
+
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.json({ text: "Hello World!" });
+});
+
+app.use("/api", globalRoutes);
+app.use("/api", paymentRoutes);
+app.use("/api", authRoutes);
+app.use("/api", courseRoutes);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
---
+++ mern-be-lms/src/models/categoryModel.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+
+const categoryModel = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+
+    courses: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Course",
+      },
+    ],
+  },
+  {
+    timestamp: true,
+  }
+);
+
+export default mongoose.model("Category", categoryModel);
