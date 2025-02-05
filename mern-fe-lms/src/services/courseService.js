898e0ab2c74db6ebac6c40d3917690181598639d
fakhri-iz <fakhriikhwnauzaki@gmail.com>
2025-02-05 15:26:37 +0700

    feat: Integrate api create course

--- mern-fe-lms/src/services/courseService.js
+++ mern-fe-lms/src/services/courseService.js
@@ -2,5 +2,15 @@ import { apiInstanceAuth } from "../utils/axios";
 
 export const getCourses = async () =>
   apiInstanceAuth.get("/courses").then((res) => res.data);
+
 export const getCategory = async () =>
   apiInstanceAuth.get("/categories").then((res) => res.data);
+
+export const createCourse = async (data) =>
+  apiInstanceAuth
+    .post("/courses", data, {
+      headers: {
+        "Content-Type": "multipart/form-data",
+      },
+    })
+    .then((res) => res.data);
